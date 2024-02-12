c098960de8025b32652a0c48232000500a6b4e04
AlejandroP75 <diego_palacio5@hotmail.com>
2024-02-12 16:52:37 -0500

    Ultimo commit por hoy

--- src/main/java/com/campuslands/services/ServiceReportes.java
+++ src/main/java/com/campuslands/services/ServiceReportes.java
@@ -1,9 +1,43 @@
 package com.campuslands.services;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
+
+import com.campuslands.models.alumno;
+import com.campuslands.models.periodo;
+import com.campuslands.models.persona;
+import com.campuslands.models.programa;
+import com.campuslands.repositories.alumnoImpl;
+import com.campuslands.repositories.periodoImpl;
+import com.campuslands.repositories.personaImpl;
+import com.campuslands.repositories.programaImpl;
+import com.campuslands.repositories.repository;
+import com.campuslands.utils.Conexion;
 
 public class ServiceReportes {
     public static Scanner leer = new Scanner(System.in);
+    @SuppressWarnings("rawtypes")
+    private static final repository programaRepositorio = new programaImpl();
+    @SuppressWarnings("unchecked")
+    static List<programa> listarProgramas = programaRepositorio.listar();
+    @SuppressWarnings("rawtypes")
+    private static final repository alumnoRepositorio = new alumnoImpl();
+    @SuppressWarnings("unchecked")
+    static List<alumno> listarAlumnos = alumnoRepositorio.listar();
+    @SuppressWarnings("rawtypes")
+    private static final repository personaRepositorio = new personaImpl();
+    @SuppressWarnings("unchecked")
+    static List<persona> listarPersonas = personaRepositorio.listar();
+    @SuppressWarnings("rawtypes")
+    private static final repository periodoRepositorio = new periodoImpl();
+    @SuppressWarnings("unchecked")
+    static List<periodo> listarPeriodos = periodoRepositorio.listar();
+
+    private static final Connection connection = Conexion.getInstance().conectar();
 
     public static void menuReportes(){
         int op = 0;
@@ -49,18 +83,121 @@ public class ServiceReportes {
         }
     }
     public static void listarEstuiantes(){
+        for (programa p : listarProgramas) {
+            System.out.println("\n" + p.getPrograma_nombre());
+            for (alumno a : listarAlumnos) {
+                if (a.getPrograma_id() == p.getPrograma_id()) {
+                    for (persona pe : listarPersonas) {
+                        if (a.getPersona_id() == pe.getPersona_id()) {
+                            System.out.println("Id: " + pe.getPersona_id() + " " + pe.getPersona_nombres() + " " + pe.getPersona_apellidos());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void calcularCostoSemestre() {
+        try {
+            for (alumno a : listarAlumnos) {
+                String sql = "SELECT SUM(t.valor_credito * a.numero_creditos) AS costo_semestre "
+                        + "FROM Matricula m " + "INNER JOIN Asignatura a ON m.id_asignatura = a.id_asignatura "
+                        + "INNER JOIN Tarifa t ON a.id_periodo = t.id_periodo " + "WHERE m.id_alumno = ?";
 
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    statement.setInt(1, a.getAlumno_id());
+                    try (ResultSet resultSet = statement.executeQuery()) {
+                        if (resultSet.next()) {
+                            double costoSemestre = resultSet.getDouble("costo_semestre");
+                            System.out.println(
+                                    "El costo del semestre para el estudiante con ID " + a.getAlumno_id() + " es: $"
+                                            + costoSemestre);
+                        } else {
+                            System.out.println(
+                                    "El estudiante con ID " + a.getAlumno_id() + " no está matriculado en ninguna asignatura.");
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    public static void calcularCostoSemestre(){
-        
+
+    public static void calcularIngresoSemestre() {
+        try {
+            for (periodo p : listarPeriodos) {
+                String sql = "SELECT SUM(t.valor_credito * a.numero_creditos) AS ingresos_semestre "
+                        + "FROM Matricula m " + "INNER JOIN Asignatura a ON m.id_asignatura = a.id_asignatura "
+                        + "INNER JOIN Tarifa t ON a.id_periodo = t.id_periodo " + "WHERE m.id_periodo = ?";
+
+                try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                    statement.setInt(1, p.getPeriodo_id());
+                    try (ResultSet resultSet = statement.executeQuery()) {
+                        if (resultSet.next()) {
+                            double ingresosSemestre = resultSet.getDouble("ingresos_semestre");
+                            System.out.println("Los ingresos de la universidad para el periodo con ID " + p.getPeriodo_id() + " son: $" + ingresosSemestre);
+                        } else {
+                            System.out.println("No hay matrículas registradas para el semestre con ID " + p.getPeriodo_id());
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    public static void calcularIngresoSemestre(){
-        
+    
+    public static void imprimirHorarioEstudiante() {
+
+        System.out.print("Ingrese el id del estudiante --> ");
+        int idAlumno = leer.nextInt();
+
+        String sql = "SELECT DISTINCT a.nombre_asignatura, h.dia, h.hora_inicio, h.hora_fin " +
+        "FROM Matricula m " +
+        "INNER JOIN Asignatura a ON m.id_asignatura = a.id_asignatura " +
+        "INNER JOIN Horario h ON a.id_asignatura = h.id_asignatura " +
+        "WHERE m.id_alumno = ?";
+            
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, idAlumno);
+            
+            try (ResultSet rs = stmt.executeQuery()) {
+                System.out.println("Horario del alumno con ID " + idAlumno + ":");
+                while (rs.next()) {
+                    String nombreAsignatura = rs.getString("nombre_asignatura");
+                    String dia = rs.getString("dia");
+                    String horaInicio = rs.getString("hora_inicio");
+                    String horaFin = rs.getString("hora_fin");
+                    String horario = dia + ", " + horaInicio + " - " + horaFin;
+                    System.out.println(nombreAsignatura + ": " + horario);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    public static void imprimirHorarioEstudiante(){
+
+    public static void imprimirMatriculadosPrograma() {
+
+        String sql = "SELECT p.nombre_programa, COUNT(*) AS num_matriculados " +
+                        "FROM Alumno a " +
+                        "INNER JOIN Programa p ON a.id_programa = p.id_programa " +
+                        "GROUP BY p.nombre_programa " +
+                        "ORDER BY num_matriculados";
+
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                System.out.println("Número de personas matriculadas en cada programa de menor a mayor:\n");
+                while (rs.next()) {
+                    String nombrePrograma = rs.getString("nombre_programa");
+                    int numMatriculados = rs.getInt("num_matriculados");
+                    System.out.println(nombrePrograma + ": " + numMatriculados + " matriculados");
+                }
+            }
         
-    }
-    public static void imprimirMatriculadosPrograma(){
-        
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
